b3ac172fcab98142abace5a25d5d3a4cbdef8b01
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2018-08-20 16:35:43 -0600

    batching scripts

---
+++ runs/slurm-batch-job.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+#SBATCH --ntasks=1
+#SBATCH --time=2:00:00
+#SBATCH --exclusive
+#
+#This recomend to minimize clashes with other users
+#SBATCH --partition=scaling
+#SBATCH --qos=normal
+#
+
+lanlytics-ssh-retry
+
+cmd_line="sed -n '$SLURM_ARRAY_TASK_ID p' $1"
+echo "get line: $cmd_line"
+
+cmd=$(eval $cmd_line)
+echo "command: $cmd"
+
+run_cmd=$(eval "echo '$cmd' | cut -d \> -f 1")
+stdout=$(eval "echo '$cmd' | cut -d \> -f 2")
+echo "run command: $run_cmd"
+echo "output: $stdout"
+
+eval $cmd
+
+#srun --output=$stdout $run_cmd
+
+PID=$(pgrep -f license.lanlytics.com)
+echo "killing ps: $PID"
+kill -15 $PID
+sleep 5
+
