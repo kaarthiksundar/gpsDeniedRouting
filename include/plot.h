9d163a93cc2681311674c616fce2a82b49824a7e
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2018-08-23 10:30:16 -0600

    plot settings

--- include/plot.h
+++ include/plot.h
@@ -27,21 +27,21 @@ void plot(const Instance & instance, const Result & result) {
 	lemon::ListGraph::NodeMap<int> colors(g);
 	lemon::ListGraph::NodeMap<int> shapes(g);
 	lemon::ListGraph::EdgeMap<int> ecolors(g);
-	lemon::ListGraph::EdgeMap<int> widths(g);
+	lemon::ListGraph::EdgeMap<double> widths(g);
 
     for (int i=0; i<instance.getNumTargets(); ++i) {
         std::tuple<double, double> coordinate = instance.getTargetCoord(i);
         coords[nodes[i]] = Point(std::get<0>(coordinate), std::get<1>(coordinate));
         shapes[nodes[i]] = 0;
-        sizes[nodes[i]] = 1;
+        sizes[nodes[i]] = 0.5;
         colors[nodes[i]] = 1;
     }
 
     for (int i=0; i<instance.getNumLandmarks(); ++i) {
         std::tuple<double, double> coordinate = instance.getLMCoord(i);
         coords[nodes[i + instance.getNumTargets()]] = Point(std::get<0>(coordinate), std::get<1>(coordinate));
-        shapes[nodes[i + instance.getNumTargets()]] = 2;
-        sizes[nodes[i + instance.getNumTargets()]] = 1;
+        shapes[nodes[i + instance.getNumTargets()]] = 0;
+        sizes[nodes[i + instance.getNumTargets()]] = 0.4;
         colors[nodes[i + instance.getNumTargets()]] = 2;
 
     }
@@ -53,16 +53,19 @@ void plot(const Instance & instance, const Result & result) {
         Node ns = nodes[s];
         Node nt = nodes[t];
         Edge temp = g.addEdge(ns, nt); 
-        widths[temp] = 1; ecolors[temp] = 3;
+        widths[temp] = 0.25; ecolors[temp] = 0;
     }
+
+    auto lmIndexes = result.getLMIndexes();
+    for (auto index : lmIndexes)
+        colors[nodes[index + instance.getNumTargets()]] = 3;
 
     graphToEps(g,"solutionPlot.eps").
 			coords(coords).
 			title("Feasible Solution Illustration").
 			nodeScale(.01).nodeSizes(sizes).
-			nodeShapes(shapes).//nodeColors(composeMap(palette, colors)).
+			nodeShapes(shapes).nodeColors(composeMap(palette, colors)).
 			edgeWidthScale(.005).edgeWidths(widths).
-            //edgeColors(composeMap(palette, ecolors)).
 			enableParallel().parArcDist(1).
 			run();
 
